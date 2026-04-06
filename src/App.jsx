1b2ce13eb9e0e51ccde589423cd2865138c867dc
JZepeda <jesus.herbey.zepeda@gmail.com>
2026-04-06 08:09:03 -0500

    updated OrdersPage.jsx and TrackingPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Routes, Route } from 'react-router';
-import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/checkout/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import "./App.css";
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
